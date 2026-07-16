0eba88a8a034c614ceff7d0cd9c98b5445baab74
Mickredset <aivliev782@gmail.com>
2026-07-16 23:38:43 +0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,19 +1,20 @@
-from flask import Flask, request, jsonify
+from http.server import BaseHTTPRequestHandler
+import json
 
-app = Flask(__name__)
 
-@app.route('/api', methods=['GET'])
-def home():
-    # Получаем параметр "user" из URL
-    name = request.args.get('name', 'guest')
-    
-    # Стандартная команда print (уйдет в логи Vercel)
-    print(f"Получен запрос для пользователя: {name}")
-    
-    # Стандартная конструкция if
-    if name == 'admin':
-        message = "Добро пожаловать в панель управления!"
-    else:
-        message = f"Привет, {name}!"
-        
-    return jsonify({"status": "success", "message": message})
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        # Устанавливаем заголовки ответа
+        self.send_response(200)
+        self.send_header('Content-type', 'application/json')
+        self.end_headers()
+
+        # Данные, которые вернет скрипт
+        response_data = {
+            "status": "success",
+            "message": "Панель управления vercel"
+        }
+
+        # Отправляем ответ на сайт
+        self.wfile.write(json.dumps(response_data).encode('utf-8'))
+        return
