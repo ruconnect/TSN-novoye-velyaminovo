e7a0baab5c7ecc6a5ddf615dd4c7bcabd40e8a17
Mickredset <aivliev782@gmail.com>
2026-07-16 23:18:33 +0300

    Create index.py

---
+++ ruconnect/index.py
@@ -0,0 +1,19 @@
+from http.server import BaseHTTPRequestHandler
+import json
+
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        # Устанавливаем заголовки ответа
+        self.send_response(200)
+        self.send_header('Content-type', 'application/json')
+        self.end_headers()
+        
+        # Данные, которые вернет скрипт
+        response_data = {
+            "status": "success",
+            "message": "Привет из Python на Vercel!"
+        }
+        
+        # Отправляем ответ на сайт
+        self.wfile.write(json.dumps(response_data).encode('utf-8'))
+        return
