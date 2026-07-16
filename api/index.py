75f055880844c7b2a5330fd60854f20a5825041f
Mickredset <aivliev782@gmail.com>
2026-07-16 23:25:51 +0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,19 +1,21 @@
 from http.server import BaseHTTPRequestHandler
 import json
 
+
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
         # Устанавливаем заголовки ответа
         self.send_response(200)
         self.send_header('Content-type', 'application/json')
         self.end_headers()
-        
+
         # Данные, которые вернет скрипт
         response_data = {
             "status": "success",
             "message": "Привет из Python на Vercel!"
         }
-        
+
         # Отправляем ответ на сайт
         self.wfile.write(json.dumps(response_data).encode('utf-8'))
+        print("Панель управления сайтом")
         return
